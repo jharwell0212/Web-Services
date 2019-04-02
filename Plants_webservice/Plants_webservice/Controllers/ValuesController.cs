f0f0fa8c271d0df740138ddd9456ced446ee536c
Jason Harwell <jha@dynamicwebusa.com>
2019-04-02 13:47:08 -0400

    Test

--- Plants_webservice/Plants_webservice/Controllers/ValuesController.cs
+++ Plants_webservice/Plants_webservice/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Plants_webservice.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]")] 
     [ApiController]
     public class ValuesController : ControllerBase
     {
